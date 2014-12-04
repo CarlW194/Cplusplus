59e0c392e07490b1c8673ca5077e46971c6ff5e7
CarlW194 <carl.white194@gmail.com>
2014-12-04 23:42:02 +0000

    racecar.h
    This file declares the methods and variables to be used in the driver.cpp file.

---
+++ Vehicles/racecar.h
@@ -0,0 +1,25 @@
+// Chapter 9 of C++ How to Program
+// racecar.h
+#ifndef RACECAR_H
+#define RACECAR_H
+
+#include "car.h"
+
+// class Racecar definition
+/* Write class header for Racecar, which inherits publicly
+   from Car */ 
+class Racecar: public Car{
+public:
+   Racecar( string n, string c, string s);
+   void setGearbox( int );
+   void useParachute();
+   void print() const;
+
+private:
+   int gearbox; // number of gears in car (e.g., 5-speed)
+   string sponsor;  
+   bool parachuteDeployed; 
+
+}; // end class Racecar
+
+#endif // RACECAR_H
