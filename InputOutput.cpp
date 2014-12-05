64dbc6ff49a9f9a7e1f9eef2269c3ac65d56735d
CarlW194 <carl.white194@gmail.com>
2014-12-05 17:32:17 +0000

    InputOutput.cpp
    This file is a simple program in C++ to get to grips with inputting and outputting variables to the user.

---
+++ InputOutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int i;
+	cout << "Please enter an integer value. ";
+	cin >> i;
+	cout << "The value oyu enter is " << i;
+	cout << " and its double is " << i*2 << ".\n\a";
+	return 0;
+}
